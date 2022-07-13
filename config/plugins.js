59261a9de4a096237c2f5c1b2d15b661300df7d3
jtraut1996 <102989547+jtraut1996@users.noreply.github.com>
2022-07-13 09:31:34 -0700

    gql

--- config/plugins.js
+++ config/plugins.js
@@ -15,14 +15,12 @@ module.exports = ({ env }) => ({
     },
   },
   graphql: {
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 100,
+    apolloServer: {
+      tracing: false,
     },
   },
   // ...
